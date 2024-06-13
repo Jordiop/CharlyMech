45826cb9c6d0a082f6a8e1bb1949cd274744ac78
carlos.sanchez <carlos.sanchez@omniaccess.com>
2024-06-13 23:34:48 +0200

    feat: navbar raw properties to test view

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,7 +1,57 @@
+import { Container } from '@mui/material';
 import '/src/styles/NavigationBar.css'
+import NavigationItem from '../models/NavigationItem';
 
 export default function NavigationBar() {
-	return(
-		<></>
+	// Temporary data for each navigation item
+	const items = [
+		new NavigationItem('Home', 'ICON', 'LINK'),
+		new NavigationItem('Skills', 'ICON', 'LINK'),
+		new NavigationItem('About', 'ICON', 'LINK'),
+		new NavigationItem('Contact', 'ICON', 'LINK'),
+	];
+
+	return (
+		<>
+			<Container maxWidth="false" sx={{
+				ul: {
+					listStyleType: 'none',
+					padding: 2,
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'space-around',
+					backgroundColor: '#393C4050',
+					borderRadius: 7.5,
+					boxShadow: 5,
+					backdropFilter: 'blur(10px)'
+				},
+				li: {
+					color: '#FAFAFA',
+					opacity: 0.5,
+					fontWeight: 'bold',
+					cursor: 'pointer',
+					'&:hover': {
+						opacity: 1,
+					},
+				},
+				width: '750px',
+				height: '100px',
+				backgroundColor: 'transparent',
+				position: 'absolute',
+				top: 0,
+				left: '50%',
+				transform: 'translateX(-50%)',
+				zIndex: 1,
+			}}>
+				<ul>{
+					items.map((item, index) => (
+						<li key={index}>
+							<span>{item.name}</span>
+						</li>
+					))
+				}</ul>
+
+			</Container >
+		</>
 	);
 }
