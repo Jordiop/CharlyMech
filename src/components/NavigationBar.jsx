eeefac108017d8c1c180814191d4a49695bbf63a
carlos.sanchez <carlos.sanchez@omniaccess.com>
2024-06-26 21:10:43 +0200

    feat: basic appbar without actions

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,8 +1,8 @@
 import { Container } from '@mui/material';
-import '/src/styles/NavigationBar.css'
-import NavigationItem from '../models/NavigationItem';
+import NavigationItem from '../models/NavigationItem'; //!
+import '../styles/NavigationBar.css'; //!
 
-export default function NavigationBar() {
+function NavigationBar() {
 	// Temporary data for each navigation item
 	const items = [
 		new NavigationItem('Home', 'ICON', 'LINK'),
@@ -13,45 +13,20 @@ export default function NavigationBar() {
 
 	return (
 		<>
-			<Container maxWidth="false" sx={{
-				ul: {
-					listStyleType: 'none',
-					padding: 2,
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'space-around',
-					backgroundColor: '#393C4050',
-					borderRadius: 7.5,
-					boxShadow: 5,
-					backdropFilter: 'blur(10px)'
-				},
-				li: {
-					color: '#FAFAFA',
-					opacity: 0.5,
-					fontWeight: 'bold',
-					cursor: 'pointer',
-					'&:hover': {
-						opacity: 1,
-					},
-				},
-				width: '750px',
-				height: '100px',
-				backgroundColor: 'transparent',
-				position: 'absolute',
-				top: 0,
-				left: '50%',
-				transform: 'translateX(-50%)',
-				zIndex: 1,
-			}}>
-				<ul>{
-					items.map((item, index) => (
-						<li key={index}>
-							<span>{item.name}</span>
-						</li>
-					))
-				}</ul>
+			<nav className="NavAppbar">
+				<Container disableGutters maxWidth={false} className="Nav">
+					<ul>{
+						items.map((item, index) => (
+							<li key={index}>
+								<span>{item.name}</span>
+							</li>
+						))
+					}</ul>
 
-			</Container >
+				</Container >
+			</nav>
 		</>
 	);
 }
+
+export default NavigationBar
