3ad00235bcf3074dcb81327e84e882be8f7ca36e
carlos.sanchez <carlos.sanchez@omniaccess.com>
2024-06-26 21:12:31 +0200

    feat: implement changes and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-// import './App.css'
-import {NavigationBar} from './components/NavigationBar';
+import { Container } from '@mui/material';
+import { NavigationBar, Footer } from './components/components.js';
+import { Home, Skills, About, Contact } from './pages/pages.js';
+import "./styles/App.css";
 
 function App() {
 
   return (
     <>
-      <NavigationBar/>
+      <Container disableGutters maxWidth="true" className="App">
+        <NavigationBar />
+        <main className="main-content">
+          <Home />
+        </main>
+        <Footer />
+      </Container >
+
     </>
   )
 }
