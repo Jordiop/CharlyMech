0d352dc3de1db44d5b916b73834d4a92b4c61b19
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-14 19:09:58 +0200

    feat: Home page component basig structure and content

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,27 @@
+import { RivePhone, CMTitle, CMSubtitle } from '../../components/components.js';
+import style from './Home.module.css';
+
+function Home() {
+	return (
+		<>
+			<section className={style.Home}>
+				<div className={style.HomeWelcomeContent}>
+					<div className={style.HomeWelcomeTitles}>
+						{/* ! Solve these routes please, is killing me */}
+						<img src="../../../assets/img/wave_hand.png" alt="Wave hand" width={150} style={{ marginTop: 20 }} />
+						<div className={style.Titles}>
+							<CMTitle style={{ backgroundColor: 'green' }} fontSize={112}>Hello</CMTitle>
+							<CMTitle fontSize={72}>world!</CMTitle>
+						</div>
+					</div>
+					<CMSubtitle>My name is Carlos, I am a mobile app software developer based in Spain with expertise in back-end development.</CMSubtitle>
+					<br />
+					<CMSubtitle>Enjoy exploring my skills, projects and experience!</CMSubtitle>
+				</div>
+				<div className={style.HomeRivePhone}><RivePhone /></div>
+			</section>
+		</>
+	);
+}
+
+export default Home
