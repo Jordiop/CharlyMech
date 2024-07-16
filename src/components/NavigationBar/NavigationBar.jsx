c636eb54039274444234cc3ad7bd3a090104c108
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-16 19:44:27 +0200

    feat: apply store to navigation bar component

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,21 +1,26 @@
 import NavigationItem from '../../models/NavigationItem'; //!
+import useNavigationStore from '../../stores/navigationStore'; //!
 import style from './NavigationBar.module.css'; //!
 
 function NavigationBar() {
 	// Temporary data for each navigation item
 	const items = [
-		new NavigationItem('Home', 'ICON', 'LINK'),
-		new NavigationItem('Skills', 'ICON', 'LINK'),
-		new NavigationItem('About', 'ICON', 'LINK'),
-		new NavigationItem('Contact', 'ICON', 'LINK'),
+		new NavigationItem('home', 'ICON', 'LINK'),
+		new NavigationItem('skills', 'ICON', 'LINK'),
+		new NavigationItem('about', 'ICON', 'LINK'),
+		new NavigationItem('contact', 'ICON', 'LINK'),
 	];
+	const { page, setPage } = useNavigationStore();
 
 	return (
 		<>
 			<div className={style.NavigationBar}>
 				<ul className={style.NavigationList}>{
 					items.map((item, index) => (
-						<li className={style.NavigationItem} key={index}>
+						<li
+							className={`${style.NavigationItem} ${page === item.name ? `${style.active}` : ''}`} key={index}
+							onClick={() => setPage(item.name)}
+						>
 							<span>{item.name}</span>
 						</li>
 					))
