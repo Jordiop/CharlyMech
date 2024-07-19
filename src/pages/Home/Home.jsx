7aa946e672bf5abd1fbcbf86320b1b01508fb1a2
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-19 14:47:47 +0200

    feat: reorder components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,20 +6,20 @@ function Home() {
 	return (
 		<>
 			<section className={style.Home}>
+				<div className={style.HomeRivePhone}><RivePhone /></div>
 				<div className={style.HomeWelcomeContent}>
 					<div className={style.HomeWelcomeTitles}>
 						{/* ! Solve these routes please, is killing me */}
-						<img src="../../../assets/img/wave_hand.png" alt="Wave hand" width={150} style={{ marginTop: 20 }} />
 						<div className={style.Titles}>
 							<CMTitle fontSize={112}>Hello</CMTitle>
 							<CMTitle fontSize={72}><TypeWriter height={72} color='#FAFAFA' /></CMTitle>
 						</div>
+						<img className={style.HomeWelcomeHand} src="../../../assets/img/wave_hand.png" alt="Wave hand" />
 					</div>
 					<CMSubtitle>My name is Carlos, I am a mobile app software developer based in Spain with expertise in back-end development.</CMSubtitle>
 					<br />
 					<CMSubtitle>Enjoy exploring my skills, projects and experience!</CMSubtitle>
 				</div>
-				<div className={style.HomeRivePhone}><RivePhone /></div>
 			</section>
 		</>
 	);
