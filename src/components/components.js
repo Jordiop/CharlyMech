f30e7c334d92be4e8626fb8446063f396756a2a1
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-21 18:00:52 +0200

    feat: update components export

--- src/components/components.js
+++ src/components/components.js
@@ -4,6 +4,8 @@ import RivePhone from './RivePhone/RivePhone';
 import CMTitle from './UIComponents/CMTitle';
 import CMSubtitle from './UIComponents/CMSubtitle';
 import TypeWriter from './UIComponents/TypeWriter';
+import TechSkill from './TechSkill/TechSkill';
+import ExpertiseLevel from './ExpertiseLevel/ExpertiseLevel';
 
 export {
 	NavigationBar,
@@ -12,4 +14,6 @@ export {
 	CMTitle,
 	CMSubtitle,
 	TypeWriter,
+	TechSkill,
+	ExpertiseLevel
 } 
