51e7ab1e69144615d3271f4c0f1a132dd95f59b9
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-07-29 15:24:16 +0200

    feat: add project

--- src/models/Project.js
+++ src/models/Project.js
@@ -29,6 +29,14 @@ const projects = [
 		"TODO",
 		[new ProjectLink(ProjectLinkType.GITHUB, 'https://github.com/CharlyMech/gengar')],
 		'/assets/img/projects/gengarapi_project_box.png'),
+	new Project(
+		'ToBook App',
+		false,
+		[languages['java']],
+		'Swing designed app that allows you to mark your favourite and read books from a catalog (OpenLibra API as source and data auto-saved in MySQL database).',
+		'TODO',
+		[new ProjectLink(ProjectLinkType.GITHUB, 'https://github.com/CharlyMech/ToBook-App')],
+		'/assets/img/projects/tobook_app.png'),
 ]
 
 export { projects };
