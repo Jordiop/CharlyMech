3a132287fbd456dac8672086a4ce09144c035e06
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-08-13 22:02:54 +0200

    feat: renew footer concept, make it minimal

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,46 @@
 import style from './Footer.module.css' //!
-import socialMedia from '../../models/SocialMedia'; //!
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import links from '/src/models/SocialMedia.js'
+import navigationItems from '../../models/NavigationItem';
+import { CMSubtitle, SeparationBar } from '../components';
 
 
 function Footer() {
 	return (
 		<>
 			<div className={style.Footer}>
-				<div className={style.SocialMedia}>
-					<ul className={style.SocialMediaList}>
-						{
-							socialMedia.map((social, index) => (
-								<li className={style.Social} key={index}>
-									<a href={social.link} target={social.target}>
-										<span><FontAwesomeIcon icon={social.icon} /></span> {social.name}
+				<div className={style.Links}>
+					<div className={style.FooterNavigation}>
+						<ul className={style.FooterNavigationList}>
+							{
+								navigationItems.map((item, index) => (
+									<span key={index} className={style.FooterNavigationItem}>
+										<CMSubtitle bold={false} fontSize={18}>
+											{item.name}
+										</CMSubtitle>
+									</span>
+								))
+							}
+						</ul>
+					</div>
+					<div className={style.SocialMedia}>
+						<ul className={style.SocialMediaList}>
+							{
+								links.map((link, index) => (
+									<a href={link.link} target={link.target} key={index} className={style.SocialMediaAnchor}>
+										<li>
+											<span className={style.Social}>
+												<link.icon />
+											</span>
+										</li>
 									</a>
-								</li>
-							))
-						}
-					</ul>
+								))
+							}
+						</ul>
+					</div>
 				</div>
+				<SeparationBar width={600} />
 				<div className={style.CopyRight}>
-					<p>© 2024 Carlos Sánchez Recio. All rights reserved.</p>
+					<CMSubtitle bold={false} fontSize={16}>© 2024 Carlos Sánchez Recio. All rights reserved.</CMSubtitle>
 				</div>
 			</div>
 		</>
