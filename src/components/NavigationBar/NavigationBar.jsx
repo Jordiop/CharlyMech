aab715a4f5092bd88ed62fe691c95d7a3d7ab272
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-08-13 22:02:27 +0200

    fix: right way for navigation items and correct on navbar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,22 +1,15 @@
-import NavigationItem from '../../models/NavigationItem'; //!
+import navigationItems from '../../models/NavigationItem'; //!
 import useNavigationStore from '../../stores/navigationStore'; //!
 import style from './NavigationBar.module.css'; //!
 
 function NavigationBar() {
-	// Temporary data for each navigation item
-	const items = [
-		new NavigationItem('home', 'ICON', 'LINK'),
-		new NavigationItem('skills', 'ICON', 'LINK'),
-		new NavigationItem('about', 'ICON', 'LINK'),
-		new NavigationItem('contact', 'ICON', 'LINK'),
-	];
 	const { page, setPage } = useNavigationStore();
 
 	return (
 		<>
 			<div className={style.NavigationBar}>
 				<ul className={style.NavigationList}>{
-					items.map((item, index) => (
+					navigationItems.map((item, index) => (
 						<li
 							className={`${style.NavigationItem} ${page === item.name ? `${style.active}` : ''}`} key={index}
 							onClick={() => setPage(item.name)}
--- src/models/NavigationItem.js
+++ src/models/NavigationItem.js
@@ -6,4 +6,11 @@ class NavigationItem {
 	}
 }
 
-export default NavigationItem;
+const navigationItems = [
+	new NavigationItem('home', 'ICON', 'LINK'),
+	new NavigationItem('skills', 'ICON', 'LINK'),
+	new NavigationItem('about', 'ICON', 'LINK'),
+	new NavigationItem('contact', 'ICON', 'LINK'),
+];
+
+export default navigationItems;
