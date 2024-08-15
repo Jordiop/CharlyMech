346eb4975384140d8cf33401da97e3e42c44a82a
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-08-15 20:21:52 +0200

    feat: implement separation bar on Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,39 +8,41 @@ function Footer() {
 	return (
 		<>
 			<div className={style.Footer}>
-				<div className={style.Links}>
-					<div className={style.FooterNavigation}>
-						<ul className={style.FooterNavigationList}>
-							{
-								navigationItems.map((item, index) => (
-									<span key={index} className={style.FooterNavigationItem}>
-										<CMSubtitle bold={false} fontSize={18}>
-											{item.name}
-										</CMSubtitle>
-									</span>
-								))
-							}
-						</ul>
+				<div className={style.FooterContent}>
+					<div className={style.Links}>
+						<div className={style.FooterNavigation}>
+							<ul className={style.FooterNavigationList}>
+								{
+									navigationItems.map((item, index) => (
+										<span key={index} className={style.FooterNavigationItem}>
+											<CMSubtitle bold={false} fontSize={18}>
+												{item.name}
+											</CMSubtitle>
+										</span>
+									))
+								}
+							</ul>
+						</div>
+						<div className={style.SocialMedia}>
+							<ul className={style.SocialMediaList}>
+								{
+									links.map((link, index) => (
+										<a href={link.link} target={link.target} key={index} className={style.SocialMediaAnchor}>
+											<li>
+												<span className={style.Social}>
+													<link.icon />
+												</span>
+											</li>
+										</a>
+									))
+								}
+							</ul>
+						</div>
 					</div>
-					<div className={style.SocialMedia}>
-						<ul className={style.SocialMediaList}>
-							{
-								links.map((link, index) => (
-									<a href={link.link} target={link.target} key={index} className={style.SocialMediaAnchor}>
-										<li>
-											<span className={style.Social}>
-												<link.icon />
-											</span>
-										</li>
-									</a>
-								))
-							}
-						</ul>
+					<div className={style.FooterSeparationBar}><SeparationBar opacity={0.6} /></div>
+					<div className={style.CopyRight}>
+						<CMSubtitle bold={false} fontSize={16}>© 2024 Carlos Sánchez Recio. All rights reserved.</CMSubtitle>
 					</div>
-				</div>
-				<SeparationBar width={600} />
-				<div className={style.CopyRight}>
-					<CMSubtitle bold={false} fontSize={16}>© 2024 Carlos Sánchez Recio. All rights reserved.</CMSubtitle>
 				</div>
 			</div>
 		</>
