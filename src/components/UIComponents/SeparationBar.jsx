4279d55df401a1f51f41b91bb086f2b0624d4189
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-08-15 20:21:29 +0200

    feat: separation bar component update

--- src/components/UIComponents/SeparationBar.jsx
+++ src/components/UIComponents/SeparationBar.jsx
@@ -1,19 +1,22 @@
 import PropTypes from 'prop-types';
 
-const SeparationBar = ({ width, height = 1, bgColor = '#989898' }) => {
+const SeparationBar = ({ width, height = 1, bgColor = '#989898', opacity = 1, className }) => {
 	const style = {
-		width: width,
+		width: width ? width : '100%',
 		height: height,
 		backgroundColor: bgColor,
+		opacity: opacity,
 	};
 
-	return <div style={style}></div>;
+	return <div style={style} className={className}></div>;
 };
 
 SeparationBar.propTypes = {
-	width: PropTypes.number.isRequired,
+	width: PropTypes.number,
 	height: PropTypes.number,
 	bgColor: PropTypes.string,
+	opacity: PropTypes.number,
+	className: PropTypes.string,
 };
 
 export default SeparationBar;
