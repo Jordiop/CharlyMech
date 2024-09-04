2f5433ca18cd46d85b75adf526d748befba883ab
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-09-04 21:38:25 +0200

    feat: responsiveness for home screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,25 @@
-import { RivePhone, CMTitle, CMSubtitle, TypeWriter } from '../../components/components.js';
+import { RivePhone, CMTitle, CMSubtitle, TypeWriter } from '/src/components/components';
 import style from './Home.module.css';
 
 function Home() {
 
 	return (
 		<>
-			<section className={style.Home}>
-				<div className={style.HomeRivePhone}><RivePhone /></div>
-				<div className={style.HomeWelcomeContent}>
-					<div className={style.HomeWelcomeTitles}>
-						{/* ! Solve these routes please, is killing me */}
-						<div className={style.Titles}>
-							<CMTitle fontSize={112}>Hello</CMTitle>
-							<CMTitle fontSize={72}><TypeWriter height={72} color='#FAFAFA' /></CMTitle>
+			<section className={style.Section}>
+				<div className={style.HomeContent}>
+					<div className={style.HomeRivePhone}><RivePhone /></div>
+					<div className={style.HomeWelcomeContent}>
+						<div className={style.HomeWelcomeTitles}>
+							<div className={style.Titles}>
+								<CMTitle fontSize={112}>Hello</CMTitle>
+								<CMTitle fontSize={72}><TypeWriter height={72} color='#FAFAFA' /></CMTitle>
+							</div>
+							<img className={style.HomeWelcomeHand} src="/assets/img/wave_hand.png" alt="Wave hand" />
 						</div>
-						<img className={style.HomeWelcomeHand} src="../../../assets/img/wave_hand.png" alt="Wave hand" />
+						<CMSubtitle>My name is Carlos, I am a mobile app software developer based in Spain with expertise in back-end development.</CMSubtitle>
+						<br />
+						<CMSubtitle>Enjoy exploring my skills, projects and experience!</CMSubtitle>
 					</div>
-					<CMSubtitle>My name is Carlos, I am a mobile app software developer based in Spain with expertise in back-end development.</CMSubtitle>
-					<br />
-					<CMSubtitle>Enjoy exploring my skills, projects and experience!</CMSubtitle>
 				</div>
 			</section>
 		</>
