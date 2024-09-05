accc98d2d913e8356f380dfa01dbe6da1bdcf5a4
CharlyMech <sanchezreciocarlos99@gmail.com>
2024-09-05 21:43:47 +0200

    fix: try directory fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
 								<CMTitle fontSize={112}>Hello</CMTitle>
 								<CMTitle fontSize={72}><TypeWriter height={72} color='#FAFAFA' /></CMTitle>
 							</div>
-							<img className={style.HomeWelcomeHand} src="assets/img/wave_hand.png" alt="Wave hand" />
+							<img className={style.HomeWelcomeHand} src="/assets/img/wave_hand.png" alt="Wave hand" />
 						</div>
 						<CMSubtitle>My name is Carlos, I am a mobile app software developer based in Spain with expertise in back-end development.</CMSubtitle>
 						<br />
